a7e9895fbf67d73d9ebf53339e8632df63ee6fab
LordKorea <lk97798@posteo.net>
2019-07-07 11:49:12 +0200

    added jar signing

--- src/main/java/io/gitlab/lordkorea/simplewaypoints/SimpleWaypointsMod.java
+++ src/main/java/io/gitlab/lordkorea/simplewaypoints/SimpleWaypointsMod.java
@@ -5,8 +5,9 @@ import net.minecraftforge.fml.common.Mod;
 /**
  * The main class of the mod.
  */
-@Mod(modid = SimpleWaypointsMod.MODID, name = SimpleWaypointsMod.MODNAME, version = SimpleWaypointsMod.VERSION)
-public class SimpleWaypointsMod {
+@Mod(modid = SimpleWaypointsMod.MODID, name = SimpleWaypointsMod.MODNAME, version = SimpleWaypointsMod.VERSION,
+        certificateFingerprint = SimpleWaypointsMod.FINGERPRINT)
+public final class SimpleWaypointsMod {
 
     /**
      * The mod ID of this mod.
@@ -22,4 +23,9 @@ public class SimpleWaypointsMod {
      * The version of this mod.
      */
     protected static final String VERSION = "@VERSION@";
+
+    /**
+     * The signing fingerprint of this mod.
+     */
+    protected static final String FINGERPRINT = "@FINGERPRINT@";
 }
