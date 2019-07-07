5d75a36009cc73b88fe52b2de5bb4df137486b16
LordKorea <lk97798@posteo.net>
2019-07-07 10:47:10 +0200

    added base structure

---
+++ src/main/java/io/gitlab/lordkorea/simplewaypoints/SimpleWaypointsMod.java
@@ -0,0 +1,25 @@
+package io.gitlab.lordkorea.simplewaypoints;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * The main class of the mod.
+ */
+@Mod(modid = SimpleWaypointsMod.MODID, name = SimpleWaypointsMod.MODNAME, version = SimpleWaypointsMod.VERSION)
+public class SimpleWaypointsMod {
+
+    /**
+     * The mod ID of this mod.
+     */
+    protected static final String MODID = "simplewaypoints";
+
+    /**
+     * The name of this mod.
+     */
+    protected static final String MODNAME = "SimpleWaypoints";
+
+    /**
+     * The version of this mod.
+     */
+    protected static final String VERSION = "@VERSION@";
+}
